9e9875404834c10ca08ac56e9a497fbb62b50064
jpscodes <jeremyps22@yahoo.com>
2023-01-23 11:01:52 -0800

    html/css board mvp ready for DOM mods

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*----- constants -----*/
+const words = [Blob, Bing, Plop, Dang, Drat, Knob, Grub]
+
+// Identify what words to use for the game by creating array of possible words.
+
+// Store pictures of suit items indexed at 0 to 7 with optonal suit iteam indexed 5, 6 and 7
+
+// Make a max number of guesses variable
+
+	/*----- state variables -----*/
+// Create board or list state variable for words to be input if correct, incorrect and for pictures to go on the suitman
+// Make a winner variable that checks to see if the player has won or lost yet
+
+	/*----- cached elements  -----*/
+// the user play button where they select a letter
+
+	/*----- event listeners -----*/
+// listen to user inputing letter in open button option that accepts user entering
+// or have selection choices for playes
+
+	/*----- functions -----*/
+// hide letters that have already been selected or return message if allowing user to enter letter that lets them know they already
+// made a choice
