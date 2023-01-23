638015af8acfef508eca2b774a5f4bd6c401cc77
jpscodes <jeremyps22@yahoo.com>
2023-01-23 13:02:00 -0800

    added random word generater and made css button style updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
 /*----- constants -----*/
-const words = [Blob, Bing, Plop, Dang, Drat, Knob, Grub]
+let words;
+words = ['Blob', 'Bing', 'Plop', 'Dang', 'Drat', 'Knob', 'Grub'];
 
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+const MAX_GUESSES = 8;
+
+let guessCount;
 // Identify what words to use for the game by creating array of possible words.
 
 // Store pictures of suit items indexed at 0 to 7 with optonal suit iteam indexed 5, 6 and 7
 
-// Make a max number of guesses variable
+
 
 	/*----- state variables -----*/
-// Create board or list state variable for words to be input if correct, incorrect and for pictures to go on the suitman
+// for the word to be chosen create math.random selection from the words arrary. Then use splice to find individual characters in the word. store these in variable called correctLetters made at the start of each game when the word is selected. use this to compare against the user guess, if they match search for the word and add it to arrary. To start no words will have repeat letters. If they don't match add letter to incorrect guesses section and update guessCount in state and on user page. Later will add pic when this happens
+secretWord = words[Math.floor(Math.random()*words.length)];
+console.log(secretWord);
+
+// if (userGuess === letter in correctLetters) add innertext to html word location and check for winner
+
+
 // Make a winner variable that checks to see if the player has won or lost yet
 
 	/*----- cached elements  -----*/
-// the user play button where they select a letter
+// let userGuess =; set to html button input that's received
+let userGuess = document.querySelector('button');
+userGuess.addEventListener('click', handleLetter)
+
+function handleLetter(evt) {
+	if (evt.target.tagName === 'BUTTON') console.log(evt.target.Id)
+}
 
 	/*----- event listeners -----*/
 // listen to user inputing letter in open button option that accepts user entering
-// or have selection choices for playes
 
 	/*----- functions -----*/
 // hide letters that have already been selected or return message if allowing user to enter letter that lets them know they already
 // made a choice
+
+function render() {
+
+}
