197253282278a15f3c78725ff58dd7cab3c70677
jpscodes <jeremyps22@yahoo.com>
2023-01-23 16:34:30 -0800

    re-did state vaiables and completed board initialization to handle clicks

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 /*----- constants -----*/
-let words;
-words = ['Blob', 'Bing', 'Plop', 'Dang', 'Drat', 'Knob', 'Grub'];
-
-const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const WORDS = ['BLOB', 'BING', 'PLOP', 'DANG', 'DRAT', 'KNOB', 'GRUB'];
 
 const MAX_GUESSES = 6;
 
-let playersGuess = null;
+
 
 // Identify what words to use for the game by creating array of possible words.
 
@@ -14,20 +11,22 @@ let playersGuess = null;
 
 
 
-	/*----- state variables -----*/
+/*----- state variables -----*/
+let secretWord;
+let guessedWord;
+let wrongLetters;
+let winner;
 // for the word to be chosen create math.random selection from the words arrary. Then use splice to find individual characters in the word. store these in variable called correctLetters made at the start of each game when the word is selected. use this to compare against the user guess, if they match search for the word and add it to arrary. To start no words will have repeat letters. If they don't match add letter to incorrect guesses section and update guessCount in state and on user page. Later will add pic when this happens
-function getSecretWord() {
-	return words[Math.floor(Math.random()*words.length)];
-}
 
 // if (userGuess === letter in correctLetters) add innertext to html word location and check for winner
 
 
 // Make a winner variable that checks to see if the player has won or lost yet
 
-	/*----- cached elements  -----*/
-	// this put my securet word in the middle of the page
-document.querySelector('.secretWord').innerHTML = getSecretWord();
+/*----- cached elements  -----*/
+const guessedEl = document.getElementById('guessed-word');
+
+
 
 // let userGuess =; set to html button input that's received
 
@@ -42,9 +41,21 @@ document.querySelector('.secretWord').innerHTML = getSecretWord();
 let userGuess = document.querySelector('main').addEventListener('click', handleLetter);
 
 function handleLetter(evt) {
-	if (evt.target.tagName !== 'BUTTON') return; 
-	console.log(evt.target.innerText);
+	// if (evt.target.tagName !== 'BUTTON') return; 
+	// console.log(evt.target.innerText);
 	playersGuess = evt.target.innerText;
+	if (wrongLetters.includes(playersGuess) || guessedWord.includes(playersGuess)) return;
+	if (secretWord.includes(playersGuess)) {
+		let newGuess = '';
+		secretWord.split('').forEach(function (char, idx) {
+			newGuess += char === playersGuess ? char : guessedWord.charAt(idx);
+		});
+		guessedWord = newGuess;
+	} else {
+		wrongLetters.push(playersGuess);
+		
+	} 
+	// update winner state
 	render();
 };
 
@@ -55,15 +66,17 @@ function handleLetter(evt) {
 	/*----- functions -----*/
 // hide letters that have already been selected or return message if allowing user to enter letter that lets them know they already
 // made a choice
-function winner() {
+function getWinner() {
 
 };
 function render() {
-
+	guessedEl.innerText = guessedWord;
 };
 
 function init() {
-	getSecretWord()
+	secretWord = WORDS[Math.floor(Math.random()*WORDS.length)];
+	wrongLetters = [];
+	guessedWord = '_'.repeat(secretWord.length);
 	winner = null;
 	render();
 };
